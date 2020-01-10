15a836f5446af629cd284b58536d26e1fae96b1c
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2020-01-10 16:11:26 +0000

    renaming App.java to Runner.java

---
+++ ie/gmit/hdip/Runner.java
@@ -0,0 +1,8 @@
+package ie.gmit.hdip;
+
+public class Runner {
+    public static void main(String[] args) {
+        FourSquareCipher fsc = new FourSquareCipher();
+        fsc.mainMenu();
+    }
+}
